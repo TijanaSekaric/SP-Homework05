137e5dd423d65a02d3cbaae28b883a259bd0e7d9
Tijana <tijana.sekaric@udg.edu.me>
2018-10-30 17:46:32 +0100

    Zadatak7

---
+++ ex7.py
@@ -0,0 +1,20 @@
+import os
+
+
+def find(path, filename):
+
+    file = os.listdir(path)
+
+    for i in file:
+
+        if os.path.isdir(path + '\\' + i):
+
+            find(path + '\\' + i, filename)
+
+        else:
+
+            if i == filename:
+                print(os.path.basename(path))
+
+
+    find("C:\\Users\\Tijana\\PycharmProjects", "ex1.py")
